9010ec0cd4b33b38285edb19728a14a0aa092b45
Rumeysa KAPLAN <rumeysaakaplan0@gmail.com>
2026-05-07 19:26:30 +0300

    feat: Responsive dashboard, sidebar layout, daily and planned task pages

---
+++ frontend/.next/dev/static/chunks/Kocluk_frontend_src_app_page_0m3sms1.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/Kocluk_frontend_0dj8w89._.js"],
+    source: "dynamic"
+});
